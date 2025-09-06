565906ce9294ebcebf05c52e15de35c4303e9634
Mickael Joseph <mickaelandriana06@gmail.com>
2025-09-06 14:49:35 +0300

    add catservice on module app

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -8,20 +8,25 @@ import {
   Redirect,
 } from '@nestjs/common';
 import { CreateCatDto } from './create-cat.dto';
+import { CatService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+
+  constructor(private readonly catService: CatService) { }
+
   @Post()
   @HttpCode(200)
   @Redirect('http://localhost:3000/cats', 302)
   createCat(@Body() catDto: CreateCatDto): CreateCatDto {
     console.log('createCat');
+    this.catService.create(catDto);
     return catDto;
   }
 
   @Get()
-  findAll(): string {
-    return 'all cats';
+  findAll(): CreateCatDto[] {
+    return this.catService.findAll()
   }
 
   @Get(':id')
