b81c530968af167f138c5ab3c224419dd1bb4fcc
Mickael Joseph <mickaelandriana06@gmail.com>
2025-09-06 17:52:52 +0300

    create basic middleware

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,17 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { CatService } from "./cats.service";
 import { CatsController } from "./cats.controller";
+import { LoggerMiddleware } from "src/logger.middleware";
 
 @Module({
   providers: [CatService],
   controllers: [CatsController]
 })
 
-export class CatModule { }
+export class CatModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('cats')
+  }
+}
